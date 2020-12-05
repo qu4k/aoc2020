dc49dcfd606d02e9ec13e51ee790568d3689cab3
Filippo Rossi <pippo.reds@gmail.com>
2020-12-05 21:53:16 +0100

    chore: format

--- day_01/solution.py
+++ day_01/solution.py
@@ -1,19 +1,21 @@
 def find_two(numbers):
-  for y in numbers:
-    for x in numbers:
-      if x + y == 2020:
-        return [x, y]
+    for y in numbers:
+        for x in numbers:
+            if x + y == 2020:
+                return [x, y]
+
 
 def find_three(numbers):
-  for z in numbers:
-    for y in numbers:
-      for x in numbers:
-        if x + y + z == 2020:
-          return [x, y, z]
+    for z in numbers:
+        for y in numbers:
+            for x in numbers:
+                if x + y + z == 2020:
+                    return [x, y, z]
+
 
 with open("input", "r") as f:
-  numbers = [int(i) for i in f.readlines()]
-  x1, y1 = find_two(numbers)
-  print(f"Part one: {x1} * {y1} = {x1 * y1}")
-  x2, y2, z2 = find_three(numbers)
-  print(f"Part two: {x2} * {y2} * {z2} = {x2 * y2 * z2}")
+    numbers = [int(i) for i in f.readlines()]
+    x1, y1 = find_two(numbers)
+    print(f"Part one: {x1} * {y1} = {x1 * y1}")
+    x2, y2, z2 = find_three(numbers)
+    print(f"Part two: {x2} * {y2} * {z2} = {x2 * y2 * z2}")
