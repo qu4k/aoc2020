c186e8587fcea65019b6a56a1e97e45079700adb
Filippo Rossi <pippo.reds@gmail.com>
2020-12-16 23:43:06 +0100

    fix(day_16): cleanup

--- day_16/solution.py
+++ day_16/solution.py
@@ -11,8 +11,8 @@ def parse_field(field: str) -> tuple:
     return (name, (a, b), (c, d))
 
 
-def inrange(v: int, r: tuple) -> bool:
-    return r[0] <= v <= r[1]
+def inrange(v: int, r1: tuple, r2: tuple) -> bool:
+    return r1[0] <= v <= r1[1] or r2[0] <= v <= r2[1]
 
 
 with open("input", "r") as f:
@@ -25,34 +25,28 @@ with open("input", "r") as f:
     for ticket in samples:
         vt = True
         for n in ticket:
-            cmp = lambda r1, r2: inrange(n, r1) or inrange(n, r2)
-            if not any(map(lambda x: cmp(*x[1:]), fields)):
+            if not any(map(lambda r: inrange(n, r[1], r[2]), fields)):
                 invalid.append(n)
                 vt = False
         if vt:
             valid.append(ticket[:])
 
-    print(sum(invalid))
+    print(f"Part one: {sum(invalid)} is the ticket scanning error rate")
 
     guesses = [fields[:] for _ in range(len(fields))]
 
     for ticket in valid:
         for i, n in enumerate(ticket):
-            cmp = lambda r1, r2: inrange(n, r1) or inrange(n, r2)
-            guesses[i] = list(filter(lambda x: cmp(*x[1:]), guesses[i]))
+            guesses[i] = list(filter(lambda r: inrange(n, r[1], r[2]), guesses[i]))
 
     guesses = [[y[0] for y in x] for x in guesses]
     order = [x[0] if len(x) == 1 else None for x in guesses]
-    change = True
 
-    while change:
-        change = False
+    while any([len(x) != 1 for x in guesses]):
         for i in range(len(guesses)):
             if len(guesses[i]) > 1:
-                change = True
                 guesses[i] = list(filter(lambda x: x not in order, guesses[i]))
         order = [x[0] if len(x) == 1 else None for x in guesses]
 
-    idx = [i for i, x in enumerate(order) if x.startswith("departure")]
-    vals = [my[i] for i in idx]
-    print(reduce((lambda x, y: x * y), vals))
+    vals = [my[i] for i, x in enumerate(order) if x.startswith("departure")]
+    print(f"Part two: {reduce((lambda x, y: x * y), vals)} are `departure` values")
